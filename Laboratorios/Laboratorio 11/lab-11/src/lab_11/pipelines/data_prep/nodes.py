79e9f051d5e378d5c8ff286014e74d49e07ef9c3
sebasurbina <sebastian.urbina@live.cl>
2023-07-10 21:18:32 -0400

    [Lab 11] Formateo codigo black8 e isort

--- Laboratorios/Laboratorio 11/lab-11/src/lab_11/pipelines/data_prep/nodes.py
+++ Laboratorios/Laboratorio 11/lab-11/src/lab_11/pipelines/data_prep/nodes.py
@@ -23,10 +23,16 @@ def _parse_money(x: pd.Series) -> pd.Series:
 
 def get_data():
     # TODO: completar get_data()
-    companies = pd.read_csv("https://github.com/MDS7202/lab_11/blob/main/companies.csv?raw=True")
-    shuttles = pd.read_excel("https://github.com/MDS7202/lab_11/blob/main/shuttles.xlsx?raw=True")
-    reviews = pd.read_csv("https://github.com/MDS7202/lab_11/blob/main/reviews.csv?raw=True")
-    
+    companies = pd.read_csv(
+        "https://github.com/MDS7202/lab_11/blob/main/companies.csv?raw=True"
+    )
+    shuttles = pd.read_excel(
+        "https://github.com/MDS7202/lab_11/blob/main/shuttles.xlsx?raw=True"
+    )
+    reviews = pd.read_csv(
+        "https://github.com/MDS7202/lab_11/blob/main/reviews.csv?raw=True"
+    )
+
     return companies, shuttles, reviews
 
 
