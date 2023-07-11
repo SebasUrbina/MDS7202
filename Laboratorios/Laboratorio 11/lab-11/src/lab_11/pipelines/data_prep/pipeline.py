31dc2c53e1c7207209d43b08e9f9f8483bcc2bf6
sebasurbina <sebastian.urbina@live.cl>
2023-07-10 21:18:50 -0400

    [Lab 11] Formateo codigo black8 e isort

--- Laboratorios/Laboratorio 11/lab-11/src/lab_11/pipelines/data_prep/pipeline.py
+++ Laboratorios/Laboratorio 11/lab-11/src/lab_11/pipelines/data_prep/pipeline.py
@@ -4,32 +4,41 @@ generated using Kedro 0.18.11
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import get_data, preprocess_companies, preprocess_shuttles, create_model_input_table
+
+from .nodes import (
+    create_model_input_table,
+    get_data,
+    preprocess_companies,
+    preprocess_shuttles,
+)
+
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([
-        node(
-            func=get_data,
-            inputs=None ,
-            outputs=["companies","shuttles","reviews"],
-            name="get_data"
-        ),
-        node(
-            func=preprocess_companies,
-            inputs="companies",
-            outputs="companies_preprocessed",
-            name="preprocess_companies"
-        ),
-        node(
-            func=preprocess_shuttles,
-            inputs="shuttles",
-            outputs="shuttles_preprocessed",
-            name="preprocess_shuttles"
-        ),
-        node(
-            func=create_model_input_table,
-            inputs=["shuttles_preprocessed","companies_preprocessed","reviews"],
-            outputs="model_input_table",
-            name="create_model_input_table"
-        )
-    ])
+    return pipeline(
+        [
+            node(
+                func=get_data,
+                inputs=None,
+                outputs=["companies", "shuttles", "reviews"],
+                name="get_data",
+            ),
+            node(
+                func=preprocess_companies,
+                inputs="companies",
+                outputs="companies_preprocessed",
+                name="preprocess_companies",
+            ),
+            node(
+                func=preprocess_shuttles,
+                inputs="shuttles",
+                outputs="shuttles_preprocessed",
+                name="preprocess_shuttles",
+            ),
+            node(
+                func=create_model_input_table,
+                inputs=["shuttles_preprocessed", "companies_preprocessed", "reviews"],
+                outputs="model_input_table",
+                name="create_model_input_table",
+            ),
+        ]
+    )
